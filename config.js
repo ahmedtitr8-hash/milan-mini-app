d8b5b525aa6b2b1536605903f2c8e72dd1dffd3a
ahmedtitr8-hash <ahmedtitr8@gmail.com>
2026-06-30 02:19:17 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL="https://ckriyvqnrzravknajckl.supabase.co";
-
-const SUPABASE_KEY="sb_publishable_rFMfYa3nWxyxp6_zYUdtCw_znCYEKfV";
-
-const supabase=window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
-);
